61c87bcd73039667895439a5d37becad59b9e7fc
whenganz <xue0808@aliyun.com>
2016-02-01 15:27:35 +0800

    Update putoutrand.bash
    加注释

--- putoutrand.bash
+++ putoutrand.bash
@@ -5,14 +5,18 @@ do
   mod=$(($2/10))
   len=${#mod}
   ((++len)) 
+  #参数2也就是最大的数字为几位数
   i=${RANDOM:1:$len}
+  #产生随机数
   if [ $i -gt $1 ] && [ $i -lt $2 ] 
+  #大于参数1，小于参数2
       then
         ((--len))
         echo $i
         for n in $(seq $len)
         do
           echo $i | grep ^0 ; [ $? -eq 0 ] && i=${i#*0}
+          #将数字前的"0"去掉
         done
         echo $i   
          break
